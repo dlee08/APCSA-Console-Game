e78d6f67bc436ba95e63bf37a3c6aeebd2d1ee2a
Retrialed <jessel40@nycstudents.net>
2024-12-12 13:16:05 -0500

    adventurer template added

--- Adventurer.java
+++ Adventurer.java
@@ -3,97 +3,79 @@
 import java.util.Random;
 
 public abstract class Adventurer{
-  private String name;
-  private int HP,maxHP;
+  private String name, specialName;
+  private int HP, maxHP, special, maxSpecial;
 
-
-  /*There is no no-arg constructor. Be careful with your subclass constructors.*/
-  
-  public Adventurer(String name){
-      this(name, 10);
+  public Adventurer(String name, String specialName){
+    this(name, specialName, 10, 10);
   }
 
-  public Adventurer(String name, int hp){
-      this.name = name;
-      this.HP = hp;
-      this.maxHP = hp;
+  public Adventurer(String name, String specialName, int hp, int special) {
+    this.name = name;
+    this.specialName = specialName;
+    this.HP = hp;
+    this.maxHP = hp;
+    this.special = special;
+    this.maxSpecial = special;
   }
 
-  //concrete method written using abstract methods.
-  //refill special resource by amount, but only up to at most getSpecialMax()
-  public int restoreSpecial(int n){
-      if( n > getSpecialMax() - getSpecial()){
-              n = getSpecialMax() - getSpecial();
-      }
-      setSpecial(getSpecial()+n);
-      return n;
-  }
-
-  //Abstract methods are meant to be implemented in child classes.
-
-  /*
-    all adventurers must have a custom special
-    consumable resource (mana/rage/money/witts etc)
-  */
-  //give it a short name (fewer than 13 characters)
-  public abstract String getSpecialName();
-  //accessor methods
-  public abstract int getSpecial();
-  public abstract void setSpecial(int n);
-  public abstract int getSpecialMax();
-
-  /*
-    all adventurers must have a way to attack enemies and
-    support their allys
-  */
-  //hurt or hinder the target adventurer
-  public abstract String attack(Adventurer other);
-
-  //heall or buff the target adventurer
-  public abstract String support(Adventurer other);
-
-  //heall or buff self
-  public abstract String support();
-
-  //hurt or hinder the target adventurer, consume some special resource
-  public abstract String specialAttack(Adventurer other);
-
-  /*
-    standard methods
-  */
-  public void applyDamage(int amount){
-    this.HP -= amount;
-  }
-
-  
-
-  //toString method
   public String toString(){
     return this.getName();
   }
 
-  //Get Methods
+  public int restoreSpecial(int n){
+    if (n > getSpecialMax() - getSpecial()){
+      n = getSpecialMax() - getSpecial();
+    }
+    setSpecial(getSpecial()+n);
+    return n;
+  }
+
   public String getName(){
     return name;
   }
 
   public int getHP(){
-      return HP;
+    return HP;
   }
 
   public int getmaxHP(){
-      return maxHP;
-  }
-  public void setmaxHP(int newMax){
-        maxHP = newMax;
+    return maxHP;
   }
 
-  //Set Methods
+  public void setmaxHP(int newMax){
+    maxHP = newMax;
+  }
+
+  public String getSpecialName() {
+    return specialName;
+  }
+
+  public int getSpecial() {
+    return special;
+  }
+  public int getSpecialMax() {
+    return maxSpecial;
+  }
+
   public void setHP(int health){
-      this.HP = health;
+    this.HP = health;
   }
 
   public void setName(String s){
-      this.name = s;
+    this.name = s;
+  }
+
+  public void setSpecial(int n) {
+    this.special = n;
+  }
+
+  public abstract String attack(Adventurer other);
+  public abstract String support(Adventurer other);
+  public abstract String support();
+  public abstract String specialAttack(Adventurer other);
+
+  public void applyDamage(int amount){
+    this.HP -= amount;
   }
 }
