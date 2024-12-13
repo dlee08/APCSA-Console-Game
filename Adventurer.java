9e43920a98862e2a6291c5774387026ab8e9b2ea
Retrialed <jessel40@nycstudents.net>
2024-12-13 13:19:18 -0500

    added some code

--- Adventurer.java
+++ Adventurer.java
@@ -4,28 +4,37 @@ import java.util.Random;
 
 public abstract class Adventurer{
   private String name, specialName;
-  private int HP, maxHP, special, maxSpecial;
+  private int HP, maxHP, special, maxSpecial, atk;
 
   public Adventurer(String name, String specialName){
-    this(name, specialName, 10, 10);
+    this(name, specialName, 100, 100, 10);
   }
 
-  public Adventurer(String name, String specialName, int hp, int special) {
+  public Adventurer(String name, String specialName, int hp, int special, int atk) {
     this.name = name;
     this.specialName = specialName;
     this.HP = hp;
     this.maxHP = hp;
     this.special = special;
     this.maxSpecial = special;
+    this.atk = atk;
   }
 
   public String toString(){
     return this.getName();
   }
 
+  public int restoreHP(int n){
+    if (n > getMaxHP() - getHP()){
+      n = getMaxHP() - getHP();
+    }
+    setHP(getHP()+n);
+    return n;
+  }
+
   public int restoreSpecial(int n){
-    if (n > getSpecialMax() - getSpecial()){
-      n = getSpecialMax() - getSpecial();
+    if (n > getMaxSpecial() - getSpecial()){
+      n = getMaxSpecial() - getSpecial();
     }
     setSpecial(getSpecial()+n);
     return n;
@@ -39,7 +48,7 @@ public abstract class Adventurer{
     return HP;
   }
 
-  public int getmaxHP(){
+  public int getMaxHP(){
     return maxHP;
   }
 
@@ -54,7 +63,7 @@ public abstract class Adventurer{
   public int getSpecial() {
     return special;
   }
-  public int getSpecialMax() {
+  public int getMaxSpecial() {
     return maxSpecial;
   }
 
@@ -77,5 +86,10 @@ public abstract class Adventurer{
 
   public void applyDamage(int amount){
     this.HP -= amount;
+  }
+
+  public void status(){
+    System.out.println(this + "'s HP is " + HP + "/" + maxHP + ".");
+    System.out.println(this + "'s " + specialName + " is " + special + "/" + maxSpecial + ".");
   }
 }
--- Wizard.java
+++ Wizard.java
@@ -1,3 +1,19 @@
-public class Wizard extends Adventurer {
-  public Wizard(String name, String specialName, 
+abstract class Wizard extends Adventurer {
+  public Wizard(String name, int hp, int special)
+    this(name, "Mana", hp, special);
+  }
+
+  public String attack(Adventurer other) {
+    other.applyDamage()
+    return this.getClass() + "has attacked 
+  }
+  public String support(Adventurer other) {
+    
+  }
+  public String support() {
+    
+  }
+  public String specialAttack(Adventurer other) {
+    
+  }
 }
