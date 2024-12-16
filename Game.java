ebb7f1a7228e32addaf3ca2c8512809db47cd385
dlee08 <davidlee03112008@gmail.com>
2024-12-16 13:19:27 -0500

    worked on char creation

--- Game.java
+++ Game.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int numCharacters = input.nextInt();
+
+    for(int i = 0; i < numCharacters; i++) {
+     createCharacter(); 
+    }
     // Add enemies below: Format: 
     // EnemyClass(String name, boolean isPlayer);
     new Wizard("Amogus", true);
@@ -8,5 +16,9 @@ public class Game {
     new Wizard("Enemy David 2", false);
 
     Active.beginGame();
+  }
+
+  public static void createCharacter() {
+    System.out.println("Please choose which class of adventurer that you want to play: ");
   }
 }
